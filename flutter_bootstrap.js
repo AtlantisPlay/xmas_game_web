c72ca8f30ca37799a79e7159319974175d9621e6
Volodymyr Mazepa <volodymyr@atlantisplay.com>
2025-11-30 03:46:50 +0200

    new build of Web version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1755435756"
+    serviceWorkerVersion: "1784585374"
   }
 });
